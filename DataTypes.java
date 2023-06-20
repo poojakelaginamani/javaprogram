5f1dfad126d0bb3d0fc4af2567bf9d1f4c0f337b
poojakelaginamani <poojapk.xworkz@gmail.com>
2023-06-20 05:53:06 -0400

    datatypes

---
+++ DataTypes.java
@@ -0,0 +1,34 @@
+public class DataTypes{
+	public static void main(String[] args){
+
+		byte =20;
+		byte priceofwaterBottel=20;
+		byte priceofkinfe=70;
+		byte priceofIDcard=50;
+		byte ageofNarendraModi=72;
+		byte priceofBook=100;
+		byte priceofpencilsBox=60;
+		byte totalNumberofCarromcoins=25;
+		byte animalBrain=32;
+		byte ageofAdityanath=51;
+		System.out.println(totalNumberofBall);
+		System.out.println(priceofwaterBottel);
+		System.out.println(priceofkinfe);
+        System.out.println(priceofIDcard);
+        System.out.println(ageofNarendraModi);
+        System.out.println(priceofBook);
+        System.out.println(priceofpencilsBox);
+        System.out.println(totalNumberofCarromcoins);
+        System.out.println(ageofAmitShah);
+        System.out.println(ageofAdityanath);
+
+
+        short  priceofbags=10000;
+        short  glassSpongeshaslive=10000;
+        short  
+        
+		
+
+
+	}
+}
