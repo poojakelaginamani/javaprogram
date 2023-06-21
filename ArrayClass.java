7687ab27c9c8248819f610444d78df8f0440282d
poojakelaginamani <137093967+poojakelaginamani@users.noreply.github.com>
2023-06-21 01:13:10 -0400

    test-push

---
+++ ArrayClass.java
@@ -0,0 +1,142 @@
+public class ArrayClass{
+	public static void main(String[] args){
+
+	int []  numberOfStudents = {10,30,40,50};
+	int []  ageOfPersons = {27,22,35,40,56};
+	int []  studentsRollNumber = {1,2,3,4,5,6};
+	int []  numberOfPlatforms = {1,2,3,4,5};
+	int []  numberOfTeachers = {5,7,8,9,10,15};
+	int []  iplTeams = {11,12,13};
+	int []  myArray = {10, 20,33,44,56,79};
+	char [] vowels = {'a','e','i','o','u'};
+	char [] names = {'p','o','o','j','a'};
+	char [] consontant = {'b','c','d','f','g','h','x','y','z'};
+	char [] divisions = {'A','B','C','D','E','F'};
+	char [] gender = {'F','M'};
+	int[] noOfSchools = {1,2,3,4,5};
+	float[] cgpa = {8.1f,8.4f,7.5f,9.1f,7.0f};
+	float[] tenthPercentage = {80.33f,88.38f,90.0f,70.32f,69.6f};
+	float[] weigth = {62.8f,70.8f,30.2f,25.6f,40.6f};
+	System.out.println(numberOfStudents[3]);
+	System.out.println(ageOfPersons[3]);
+	System.out.println(studentsRollNumber[3]);
+	System.out.println(numberOfPlatforms[3]);
+	System.out.println(numberOfTeachers[4]);
+	System.out.println(iplTeams[0]);
+	System.out.println(divisions[3]);
+	System.out.println(tenthPercentage[3]);
+	System.out.println(weigth[3]);
+	System.out.println(myArray[1]);
+	System.out.println(vowels[2]);
+	System.out.println(noOfSchools[3]);
+	System.out.println(gender[0]);
+	System.out.println(consontant[5]);
+	System.out.println(names[3]);
+	System.out.println(divisions[3]);
+	System.out.println(tenthPercentage[3]);
+	System.out.println(weigth[3]);
+    int[] studentUSN = new int[5];
+    studentUSN[0] = 22;
+    studentUSN[1] = 29;
+    studentUSN[2] = 49;
+    studentUSN[3] = 02;
+    studentUSN[4] = 35;
+    int[] noOfFloors = new int[5];
+	noOfFloors[0] = 1;
+	noOfFloors[1] = 2;
+	noOfFloors[2] = 3;
+	noOfFloors[3] = 4;
+	noOfFloors[4] = 5;
+	int[] lengthOfPipes = new int[5];
+	lengthOfPipes[0] = 10;
+	lengthOfPipes[1] = 20;
+	lengthOfPipes[2] = 30;
+	lengthOfPipes[3] = 40;
+	lengthOfPipes[4] = 50;
+	int[] phoneNumber = new int[5];
+	phoneNumber[0] = 98;
+	phoneNumber[1] = 26;
+	phoneNumber[2] = 35;
+	phoneNumber[3] = 56;
+	phoneNumber[4] = 60;
+	char[] rainbowColors = new char[7];
+	rainbowColors[0] = 'r';
+	rainbowColors[1] = 'o';
+	rainbowColors[2] = 'y';
+	rainbowColors[3] = 'g';
+	rainbowColors[4] = 'b';
+	rainbowColors[5] = 'i';
+	rainbowColors[6] = 'v';
+	float[] temperature = new float[5];
+	temperature[0] = 24.7f;
+	temperature[1] = 33.0f;
+	temperature[2] = 44.7f;
+	temperature[3] = 56.2f;
+	temperature[4] = 24.2f;
+	temperature[1] = 40.0f;
+    char[] primeMinister = new char[10];
+    primeMinister[0] = 'N';
+    primeMinister[1] = 'A';
+    primeMinister[2] = 'R';
+    primeMinister[3] = 'E';
+    primeMinister[4] = 'N';
+    primeMinister[5] = 'D';
+    primeMinister[5] = 'R';
+    primeMinister[7] = 'M';
+    primeMinister[8] = '0';
+    primeMinister[9] = 'D';
+    primeMinister[10] = 'I';
+    char[] ourCountry = new char[5];
+    ourCountry[0] = 'I';
+    ourCountry[0] = 'N'; 
+    ourCountry[0] = 'D';
+    ourCountry[0] = 'I';
+    ourCountry[4] = 'A';
+    float[] fruitsWeigth = new float[5];
+	fruitsWeigth[0] = 20.5f;
+	fruitsWeigth[1] = 10.5f;
+	fruitsWeigth[2] = 11.5f;
+	fruitsWeigth[3] = 15.5f;
+	fruitsWeigth[4] = 22.8f;
+	float[] hieghtOfPeoples = new float[5];
+	hieghtOfPeoples[0] = 2.6F;
+	hieghtOfPeoples[1] = 3.7F;
+	hieghtOfPeoples[2] = 4.4F;
+	hieghtOfPeoples[3] = 5.1F;
+	hieghtOfPeoples[4] = 6.0F;
+	
+for (int i=0; i<studentUSN.length;i++) {
+System.out.println(studentUSN[2]);	
+}
+for (int i=10; i<primeMinister.length;i--) {
+System.out.println(primeMinister[7]);	
+}
+for (int i=0; i<rainbowColors.length;i++) {
+System.out.println(rainbowColors[6]);	
+}
+for (int i=0; i<ourCountry.length;i++) {
+System.out.println(ourCountry[2]);	
+}
+for (int i=0; i<temperature.length;i++) {
+System.out.println(temperature[2]);	
+}
+
+System.out.println(noOfFloors[0]);
+System.out.println(lengthOfPipes[4]);
+System.out.println(phoneNumber[3]);
+System.out.println(fruitsWeigth[4]);
+System.out.println(hieghtOfPeoples[4]);
+
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
