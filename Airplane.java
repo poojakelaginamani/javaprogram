cf1f1f3bec03e7daab3a9f69db53afe87de93f18
poojakelaginamani <137093967+poojakelaginamani@users.noreply.github.com>
2023-07-04 10:16:44 +0530

    object-class

---
+++ Airplane.java
@@ -0,0 +1,88 @@
+public class Airplane{
+//States
+
+String brand;
+int speed;
+double size;
+String color;
+String pilot;
+String destination;
+int model;
+String make;
+int year;
+int cockpit;
+long weight;
+long maxPassengers;
+String smartTv;
+String ac;
+String foodFacility;
+int light;
+int chairs;
+int wings;
+int landingGears;
+
+public Airplane(String brand,int speed,double size,String color,String pilot,String destination,int model,String make,int year,int cockpit,long weight,long maxPassengers,int wings,int landingGears){
+this.brand=brand;
+this.speed=speed;
+this.size=size;
+this.color=color;
+this.pilot=pilot;
+this.destination=destination;
+this.model=model;
+this.make=make;
+this.year=year;
+this.cockpit=cockpit;
+this.weight=weight;
+this.maxPassengers=maxPassengers;
+this.wings=wings;
+this.landingGears=landingGears;
+}
+public static void onAc(){
+
+}
+public static void fightControls(){
+
+}
+public static void onWinglet(){
+
+}
+public static void compressingAir(){
+
+}
+public static void fuselage(){
+
+}
+public static void startEngine(){
+
+}
+public static void charge(){
+
+}
+public static void toWatchTv(){
+
+}
+public static void sideConsoles(){
+
+}
+public static void horizontalStabilizer(){
+
+}
+public static void main(String[] args) {
+	Airplane Airplane = new Airplane("Boeing Co",230,56.3d,"White","male","American",737,"Leading global aerospace",1916,2,187000l,3850l,4,3);
+	System.out.println(Airplane.brand);
+	System.out.println(Airplane.speed);
+	System.out.println(Airplane.size);
+	System.out.println(Airplane.color);
+	System.out.println(Airplane.destination);
+	System.out.println(Airplane.model);
+	System.out.println(Airplane.make);
+	System.out.println(Airplane.year);
+	System.out.println(Airplane.weight);
+	System.out.println(Airplane.maxPassengers);
+	System.out.println(Airplane.wings);
+	System.out.println(Airplane.landingGears);
+	System.out.println(Airplane.cockpit);
+	
+	
+}
+}
